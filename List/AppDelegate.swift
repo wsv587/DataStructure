b04bfe1381afc60c4ca041307fce72a56c18fcab
王松 <songwangs@pptv.com>
2018-05-15 22:14:59 +0800

    Initial Commit

---
+++ List/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  List
+//
+//  Created by sw on 2018/5/15.
+//  Copyright © 2018年 ws. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ List/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  List
+//
+//  Created by sw on 2018/5/15.
+//  Copyright © 2018年 ws. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
